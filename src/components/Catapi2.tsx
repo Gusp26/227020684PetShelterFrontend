9d5cd69ae3103227c80ebb6a77a09f4f79107ef1
Gusp26 <125976460+Gusp26@users.noreply.github.com>
2023-05-30 12:43:50 +0000

    updated

--- src/components/Catapi2.tsx
+++ src/components/Catapi2.tsx
@@ -10,7 +10,7 @@ const Catapi2 = ({ Catapi2 }) => {
                   <p>{Catapi2?.breeds[0]?.description}</p>
                   <Image width={300} height={300} src={Catapi2.url} />
                   <p></p>
-                  <a className="text-muted" href={`${Catapi2?.breeds[0]?.wikipedia_url}`}>
+                  <a className="text-muted" href={`${Catapi2?.breeds[0]?.wikipedia_url}`} target="_blank">
                     Wikipedia
                   </a>
                 </Card>
